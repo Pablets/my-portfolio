c6cfec32b6e5eac2bf4bb80d52edd742ecfea9f5
Pablets <godoypablom@gmail.com>
2021-02-05 11:18:28 -0300

    Fixing dar elements

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -52,7 +52,7 @@ const NavBar = () => {
                 <button
                   onClick={() => setToogle(!toogle)}
                   type="button"
-                  class="group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  class="group bg-transparent rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-white focus:outline-none"
                 >
                   <span>Solutions</span>
                   {/* <!--
@@ -61,7 +61,7 @@ const NavBar = () => {
               Item active: "text-gray-600", Item inactive: "text-gray-400"
             --> */}
                   <svg
-                    class="ml-2 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                    class="ml-2 h-5 w-5 text-gray-400 group-hover:text-white"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -94,7 +94,7 @@ const NavBar = () => {
                   } absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2`}
                 >
                   <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                    <div class="relative grid gap-6 bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
                       <a class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-600">
                         {/* <!-- Heroicon name: outline/chart-bar --> */}
                         <svg
@@ -113,7 +113,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Analytics
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -141,7 +141,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Engagement
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -169,7 +169,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Security
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -196,7 +196,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Integrations
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -224,7 +224,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Automations
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -236,7 +236,7 @@ const NavBar = () => {
                     </div>
                     <div class="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
                       <div class="flow-root">
-                        <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                        <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-500 hover:bg-gray-100">
                           {/* <!-- Heroicon name: outline/play --> */}
                           <svg
                             class="flex-shrink-0 h-6 w-6 text-gray-400"
@@ -264,7 +264,7 @@ const NavBar = () => {
                       </div>
 
                       <div class="flow-root">
-                        <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                        <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-500 hover:bg-gray-100">
                           {/* <!-- Heroicon name: outline/phone --> */}
                           <svg
                             class="flex-shrink-0 h-6 w-6 text-gray-400"
@@ -289,19 +289,19 @@ const NavBar = () => {
                 </div>
               </div>
 
-              <a class="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a class="text-base font-medium text-gray-500 hover:text-gray-500">
                 Pricing
               </a>
-              <a class="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a class="text-base font-medium text-gray-500 hover:text-gray-500">
                 Docs
               </a>
 
               <div class="relative">
-                {/* <!-- Item active: "text-gray-900", Item inactive: "text-gray-500" --> */}
+                {/* <!-- Item active: "text-gray-500", Item inactive: "text-gray-500" --> */}
                 <button
                   onClick={() => setMore(!more)}
                   type="button"
-                  class="group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  class="group bg-transparent rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-white focus:outline-none"
                 >
                   <span>More</span>
                   {/* <!--
@@ -310,7 +310,7 @@ const NavBar = () => {
               Item active: "text-gray-600", Item inactive: "text-gray-400"
             --> */}
                   <svg
-                    class="ml-2 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                    class="ml-2 h-5 w-5 text-gray-400 group-hover:text-white"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -342,7 +342,7 @@ const NavBar = () => {
                   } absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2`}
                 >
                   <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                    <div class="relative grid gap-6 bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
                       <a class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-600">
                         {/* <!-- Heroicon name: outline/support --> */}
                         <svg
@@ -361,7 +361,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Help Center
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -389,7 +389,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Guides
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -417,7 +417,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Events
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -445,7 +445,7 @@ const NavBar = () => {
                           />
                         </svg>
                         <div class="ml-4">
-                          <p class="text-base font-medium text-gray-900">
+                          <p class="text-base font-medium text-gray-500">
                             Security
                           </p>
                           <p class="mt-1 text-sm text-gray-500">
@@ -454,7 +454,7 @@ const NavBar = () => {
                         </div>
                       </a>
                     </div>
-                    <div class="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
+                    <div class="px-5 py-5 bg-gray-800 sm:px-8 sm:py-8">
                       <div>
                         <h3 class="text-sm tracking-wide font-medium text-gray-500 uppercase">
                           Recent Posts
@@ -492,7 +492,7 @@ const NavBar = () => {
               </div>
             </nav>
             <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+              <a class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-500">
                 Sign in
               </a>
               <a class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
@@ -661,7 +661,8 @@ const NavBar = () => {
               </div>
             </div>
             <div class="py-6 px-5 space-y-6 border-none">
-              <div class="grid grid-cols-2 gap-y-4 gap-x-8"><a class="text-base font-medium text-gray-100 hover:text-white">
+              <div class="grid grid-cols-2 gap-y-4 gap-x-8">
+                <a class="text-base font-medium text-gray-100 hover:text-white">
                   Pricing
                 </a>
 
